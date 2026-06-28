e0132180957f7b3da6ed1001f4836387359c52de
kwakseobang <iii6602@naver.com>
2026-06-28 21:19:52 +0900

    git Initialize

---
+++ src/main/java/org/kwakmunsu/fancafe/FancafeApplication.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.fancafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FancafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kwakmunsu/fancafe/FancafeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.fancafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
